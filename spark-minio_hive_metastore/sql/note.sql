6eec094e5114158142ecb0d5a6913c91cc574a83
Nguyễn Duy Thắng <thangnd@etc.vn>
2026-01-16 15:37:06 +0700

    etl

--- spark-minio_hive_metastore/sql/note.sql
+++ spark-minio_hive_metastore/sql/note.sql
@@ -4,3 +4,4 @@ INNER JOIN ice.gold.fact_transport_transaction_stage b
 ON a.transport_trans_id  = b.transport_trans_id
 INNER JOIN ice.gold.dim_toll_stage c
 on a.stage_id = c.stage_id
+on a.stage_id = c.stage_id
